7c227cbaa8b5f483e04bf7a6f1f129bd6d6e9a22
Diego <diego.lnadaeta.r@gmail.com>
2020-11-23 19:50:21 +0100

    Capital Letters

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-import classes from "./Button.module.scss";
-import Icon from "../icon/Index";
+import classes from "./button.module.scss";
+import Icon from "../icon/index";
 
 function Button({
     icon,
