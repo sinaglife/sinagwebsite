12525ea2fbda61565658abed7b64904075410dd1
Diego <diego.lnadaeta.r@gmail.com>
2021-01-25 22:53:51 +0100

    Feat. Logo size when scrolling down

--- src/components/UI/icon/index.js
+++ src/components/UI/icon/index.js
@@ -1,4 +1,4 @@
-Nimport React from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
--- src/components/header/Toolbar.js
+++ src/components/header/Toolbar.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import classes from "./Toolbar.module.scss";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "@reach/router";
 import Logo from "./logosinagvectoriced.png";
-import Button from "../UI/button/Button";
+
+const LogoHeader = () => {
+  const [isShrunk, setShrunk] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      setShrunk((isShrunk) => {
+        if (
+          !isShrunk &&
+          (document.body.scrollTop > 70 ||
+            document.documentElement.scrollTop > 70)
+        ) {
+          return true;
+        }
+
+        if (
+          isShrunk &&
+          document.body.scrollTop < 70 &&
+          document.documentElement.scrollTop < 70
+        ) {
+          return false;
+        }
+
+        return isShrunk;
+      });
+    };
+
+    window.addEventListener("scroll", handler);
+    return () => window.removeEventListener("scroll", handler);
+  }, []);
+
+  return (
+    <Link to="/" className={classes.toolbar__logo}>
+      <img src={Logo} alt="SinagVibes&Designs" className={isShrunk ? classes.shrunk : null}/>
+    </Link>
+  )
+};
 
 const Toolbar = (props) => (
   <div className={classes.toolbar}>
@@ -13,12 +49,10 @@ const Toolbar = (props) => (
         <MenuIcon />
       </IconButton>
     </div>
-    <Link to="/" className={classes.toolbar__logo}>
-      <img src={Logo} alt="SinagVibes&Designs" />
-    </Link>
+    <LogoHeader/>
     <div className={classes.toolbar__rightContainer}>
-      {/* <Button icon="search" color="black" size="medium" padding="noPadding" /> */}
-      {/* <Button icon="heart" color="black" size="medium" padding="noPadding" /> */}
+    <span>CA</span>
+    <span>mu</span>
     </div>
   </div>
 );
